e6ccb09cbadf68c6a8d2fa52840be52d8fff4513
n1sym <owlbook248@gmail.com>
2022-11-19 01:32:07 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
 import { cards } from "./cards/api"
+import { cors } from 'hono/cors'
 
 const app = new Hono();
+app.use('/api/*', cors({origin: ['http://localhost:3000', 'https://animecard.pages.dev', 'https://animecard.n1sym.com']}))
 app.route("/api/cards", cards)
 
 export default app;
